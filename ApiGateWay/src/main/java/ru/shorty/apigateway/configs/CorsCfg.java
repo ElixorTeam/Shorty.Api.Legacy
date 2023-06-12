e0dce03a7534360384d45d3bb32217101cc36d03
BaggerFast <riosha3@gmail.com>
2023-06-12 21:29:55 +0300

    UPDATE: create application properties (default, dev)

--- ApiGateWay/src/main/java/ru/shorty/apigateway/configs/CorsCfg.java
+++ ApiGateWay/src/main/java/ru/shorty/apigateway/configs/CorsCfg.java
@@ -1,35 +1,43 @@
 package ru.shorty.apigateway.configs;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
-import java.util.List;
-
 @Configuration
 public class CorsCfg {
 
-    @Value("${FRONT_URL:localhost}")
-    private String frontUrl;
+    private final Environment environment;
+
+    public CorsCfg(Environment environment) {
+        this.environment = environment;
+    }
 
     @Bean
-    public CorsWebFilter corsFilter()
-    {
+    public CorsWebFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
+
+        if (isDevActive())
+            config.addAllowedOrigin("*");
+
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
-        config.setAllowedOrigins(List.of(
-                String.format("http://%s:3031", frontUrl),
-                String.format("http://%s:3000", frontUrl),
-                String.format("http://en.%s:3000", frontUrl),
-                String.format("http://ru.%s:3000", frontUrl)
-        ));
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return new CorsWebFilter(source);
+    }
+
+    private boolean isDevActive() {
+        String[] activeProfiles = environment.getActiveProfiles();
+
+        for (String profile : activeProfiles)
+            if (profile.equalsIgnoreCase("dev"))
+                return true;
+        return false;
     }
 
 }
