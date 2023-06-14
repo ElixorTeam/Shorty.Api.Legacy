a24cef1bd25faa281b9637f3ef8918a4fa122e73
BaggerFast <riosha3@gmail.com>
2023-06-14 15:05:19 +0300

    ADD: CustomOAuth2User - github

---
+++ LinkShortener/src/main/java/ru/shorty/linkshortener/configs/SecurityConfig.java
@@ -0,0 +1,42 @@
+package ru.shorty.linkshortener.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import ru.shorty.linkshortener.oauth2.CustomOAuth2UserService;
+import ru.shorty.linkshortener.oauth2.OAuth2LoginSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final CustomOAuth2UserService oauth2UserService;
+
+    private final OAuth2LoginSuccessHandler oauth2LoginSuccessHandler;
+
+    public SecurityConfig(CustomOAuth2UserService oauth2UserService, OAuth2LoginSuccessHandler oauth2LoginSuccessHandler) {
+        this.oauth2UserService = oauth2UserService;
+        this.oauth2LoginSuccessHandler = oauth2LoginSuccessHandler;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .authorizeHttpRequests()
+                .requestMatchers("/api/**").authenticated()
+                .anyRequest().permitAll()
+            .and()
+            .oauth2Login()
+                .userInfoEndpoint()
+                .userService(oauth2UserService)
+            .and()
+            .successHandler(oauth2LoginSuccessHandler)
+            .and()
+            .logout().permitAll();
+        return http.build();
+    }
+
+}
